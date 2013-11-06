a863e2ca287ec8a19675e7bd1aefcb7a9a440488
Javier Azcurra <javier2mil3@gmail.com>
2013-11-06 17:45:55 -0300

    se arregla busqueda en comentarios

--- src/VentanasPaneles/TablaBuscador.java
+++ src/VentanasPaneles/TablaBuscador.java
@@ -290,6 +290,10 @@ public class TablaBuscador extends javax.swing.JPanel {
         }
 
         String text = modeloTabla.getValueAt(row, column).toString();
+        
+        text = text.trim().replaceAll("\n", "");
+        
+        text = text.replaceAll("\t", "");
 
         codigoPanel.setHighlight(text);        
         
